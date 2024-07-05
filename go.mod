b1cd3750775fbe49617ffb2e01ce0e8b84498c5c
Zelalem Tamrie Bantayehu <zelalem.t8@gmail.com>
2024-07-05 11:54:57 +0400

    Initial commit

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
-module addispay
+module github.com/zelalem-t8/addispay
 
 go 1.21.6
-
-require (
-	github.com/subosito/gotenv v1.6.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
-)
---
+++ test/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yourusername/addispay"
+)
+
+func main() {
+	addisPay := addispay.New("your_base64_public_key", "your_base64_private_key", "your_auth_token")
+
+	response, err := addisPay.SendRequest("1000", "12345", "ETB", "John", "john@example.com", "0912345678", "Doe", "30", "unique_nonce", "https://your.notify.url", "https://your.return.url", "Test transaction")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Response:", response.Status)
+	}
+}
